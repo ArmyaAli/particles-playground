05ecbfa441080f35e7879653dab14f827761f425
Ali Umar <ali.umar.work@gmail.com>
2021-12-02 12:25:08 -0500

    we can now add particles on every click

--- main.c
+++ main.c
@@ -29,13 +29,19 @@ int main(void)
         if (IsMouseButtonPressed(0))
         {
             printf("Clicked\r\n");
+            log_particles(buffer);
+            InitParticles(buffer);
+
         }
 
+        Update(buffer);
         Draw(buffer);
-
+        remove_finished_particles(buffer);
+    
         EndDrawing();
     }
 
+    destroy_particle_buffer(buffer);
     CloseWindow();
 
     return 0;
@@ -52,7 +58,7 @@ void InitParticles(PARTICLE_BUFFER* buffer)
     int i;
     for (i = 0; i < SIZE; ++i)
     {
-        Particle p = {{rand() % 801, rand() % 401}, 0, 0, 255, colors[rand() % 3]};
+        Particle p = {{rand() % 801, rand() % 401}, 0, 0, rand() % 500, colors[rand() % 3]};
         add_particle(buffer, &p);
     }
 }
@@ -61,13 +67,21 @@ void InitParticles(PARTICLE_BUFFER* buffer)
 void Update(PARTICLE_BUFFER* buffer)
 {
     Particle* pArray = buffer->particles;
+    int i;
+
+    for (i = 0; i < buffer->size; ++i) {
+        if(pArray[i].lifeTime > 0)
+            pArray[i].lifeTime--;
+    }
+
+
 }
 
 void Draw(PARTICLE_BUFFER* buffer)
 {
     Particle* pArray = buffer->particles;
     int i;
-    for (i = 0; i < SIZE; ++i) {
+    for (i = 0; i < buffer->size; ++i) {
         DrawCircle(pArray[i].pos.x, pArray[i].pos.y, RADIUS, pArray[i].color);
     }
 }
