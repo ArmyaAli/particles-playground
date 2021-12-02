850b045f503e489f9c0ce46a5f33ded964f762e9
Ali Umar <ali.umar.work@gmail.com>
2021-12-02 10:50:41 -0500

    added logging fn

--- include/particleBuffer.h
+++ include/particleBuffer.h
@@ -1,11 +1,32 @@
-#ifndef PBUFFER
-#define PBUFFER
+#ifndef P_BUFFER
+#define P_BUFFER
 
-typedef struct _particle Particle;
+#include <stdlib.h>
 
-typedef struct _pBuffer {
-    size_t size;
+typedef struct _particle
+{
+    struct _position
+    {
+        int x;
+        int y;
+    } pos;
+
+    float vel;
+    float acceleration;
+    float lifeTime;
+} Particle;
+
+typedef struct _pBuffer
+{
+    int size;
+    int capacity;
     Particle* particles;
 } PARTICLE_BUFFER;
+
+PARTICLE_BUFFER* init();
+void add_particle(PARTICLE_BUFFER*, Particle*);
+void remove_finished_particles(PARTICLE_BUFFER*);
+void log_particles(PARTICLE_BUFFER*);
+void destroy_particle_buffer(PARTICLE_BUFFER*);
 
 #endif
