047f308af68c888f94f0c42e364dec388d216295
Ali Umar <ali.umar.work@gmail.com>
2021-12-02 23:10:24 -0500

    created menu stuff for some buttons

---
+++ include/menu.h
@@ -0,0 +1,10 @@
+#ifndef MENU
+#define MENU
+
+#include "../include/raylib.h"
+
+void draw_menu();
+void update_menu();
+int check_button_collision();
+
+#endif
---
+++ src/menu.c
@@ -0,0 +1,6 @@
+#include "../include/menu.h";
+
+void draw_menu()
+{
+    DrawRectangle(0, 0, 100, 50, GRAY);
+}
