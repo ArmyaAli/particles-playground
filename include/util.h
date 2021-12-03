7f4dcd10d6840fa4fe97407e9016da7d3750caf4
Ali Umar <ali.umar.work@gmail.com>
2021-12-02 23:23:46 -0500

    added utility files

---
+++ include/util.h
@@ -0,0 +1,8 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdlib.h>
+
+float float_rand( float min, float max );
+
+#endif
---
+++ src/util.c
@@ -0,0 +1,7 @@
+#include "../include/util.h"
+
+float float_rand( float min, float max )
+{
+    float scale = rand() / (float) RAND_MAX; /* [0, 1.0] */
+    return min + scale * ( max - min );      /* [min, max] */
+}
