b6e7cbe765dddd8bc2f693273333fe8d824609f6
Ali Umar <ali.umar.work@gmail.com>
2021-12-02 23:24:16 -0500

    basic button templates for menu

--- src/menu.c
+++ src/menu.c
@@ -1,6 +1,18 @@
 #include "../include/menu.h";
 
+#define MENU_X 32
+#define MENU_Y 64
+
+void update_menu() 
+{
+
+}
+
 void draw_menu()
 {
-    DrawRectangle(0, 0, 100, 50, GRAY);
+    const int buttonGap = 100;
+    int i;
+    for(i = 0; i < 3; ++i) {
+        DrawRectangle(MENU_X, MENU_Y + i * buttonGap, 100, 50, GRAY);
+    }
 }
