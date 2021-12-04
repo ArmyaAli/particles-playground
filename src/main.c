dafdb2d13bb155466861fc01a2297ce291bebd30
Ali Umar <ali.umar.work@gmail.com>
2021-12-04 14:33:00 -0500

    cleanup main

--- src/main.c
+++ src/main.c
@@ -13,18 +13,13 @@ int main(void)
     // RAYLIB INIT
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Ali's Particle Playground");
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
-    // Buffer
-    char particleNumberText[32];
+    
     PARTICLE_BUFFER* buffer = init_particles_buffer();
 
     while (!WindowShouldClose())
     {
-        // sprintf(particleNumberText, "# of Particles: %d", buffer->size);
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
-        DrawFPS(8, 8);
-        // DrawText(particleNumberText, SCREEN_WIDTH - MeasureText(particleNumberText, 24) - 8, 8, 24, RED);
         Create(buffer);
         Update(buffer);
         Draw(buffer);
