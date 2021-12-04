6ffe59949964a3a46a987b2ccb4bc5ac4cd0f105
Ali Umar <ali.umar.work@gmail.com>
2021-12-04 14:33:08 -0500

    added text stuff

--- src/lifecycle.c
+++ src/lifecycle.c
@@ -1,5 +1,7 @@
 #include "../include/lifecycle.h";
 
+char particleNumberText[32];
+
 void Create(PARTICLE_BUFFER* buffer)
 {
     if(IsMouseButtonDown(0) && !collision_exists())
@@ -8,12 +10,16 @@ void Create(PARTICLE_BUFFER* buffer)
 
 void Update(PARTICLE_BUFFER* buffer)
 {
+
+    sprintf(particleNumberText, "# of Particles: %d", buffer->size);
     update_particles(buffer); 
     update_menu();
 }
 
 void Draw(PARTICLE_BUFFER* buffer)
 {
+    DrawFPS(8, 8);
+    DrawText(particleNumberText, 800 - MeasureText(particleNumberText, 24) - 8, 8, 24, RED);
     draw_particles(buffer);
     draw_menu();
 }
