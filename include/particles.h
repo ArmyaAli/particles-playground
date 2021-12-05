fe6943eef8c147f91e74bcfd4dd0a0117b6d1071
Ali Umar <ali.umar.work@gmail.com>
2021-12-05 12:10:04 -0500

    added some interface functions

--- include/particles.h
+++ include/particles.h
@@ -3,12 +3,19 @@
 
 #include "../include/raylib.h"
 #include "../include/particleBuffer.h"
+#include "../include/menu.h"
 
 #define RADIUS 2
 #define SIZE   32
 
 void create_particles(PARTICLE_BUFFER*);
 void update_particles(PARTICLE_BUFFER*);
+void lifetime_stuff(Particle*, int);
+void apply_physics_quantities(Particle*, int);
+void check_bounds(Particle*, int);
+void change_color_on_button_click();
+void change_velocity_on_button_click();
 void draw_particles(PARTICLE_BUFFER*);
 void destroy_particles(PARTICLE_BUFFER*);
+
 #endif
