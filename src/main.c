85cc00a73e544ae8417f18695363421fc9472176
Ali Umar <ali.umar.work@gmail.com>
2021-12-10 21:00:36 -0500

    Added init fn

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
+#define RAYGUI_IMPLEMENTATION
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
 #include "../include/raylib.h"
 #include "../include/particleBuffer.h"
+#include "../include/raygui.h"  
 
 #define SCREEN_WIDTH  800
 #define SCREEN_HEIGHT 400
@@ -14,8 +17,8 @@ int main(void)
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Ali's Particle Playground");
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     
-    PARTICLE_BUFFER* buffer = init_particles_buffer();
-
+    ParticleBuffer* buffer = init_particles_buffer();
+    Init();
     while (!WindowShouldClose())
     {
         BeginDrawing();
