b5b03c0e5895ee1fa15a118de8e4840b3a0b5eba
Ali Umar <ali.umar.work@gmail.com>
2021-12-11 11:21:49 -0500

    rename ParticleBuffer -> ParticleList

--- include/lifecycle.h
+++ include/lifecycle.h
@@ -2,15 +2,15 @@
 #define LIFECYCLE_H
 
 #include "../include/raylib.h"
-#include "../include/particleBuffer.h"
+#include "../include/ParticleList.h"
 #include "../include/util.h"
 #include "../include/particles.h"
 #include "../include/menu.h"
 
 void Init();
-void Create(ParticleBuffer*);
-void Update(ParticleBuffer*);
-void Draw(ParticleBuffer*);
-void Destroy(ParticleBuffer*);
+void Create(ParticleList*);
+void Update(ParticleList*);
+void Draw(ParticleList*);
+void Destroy(ParticleList*);
 
 #endif
