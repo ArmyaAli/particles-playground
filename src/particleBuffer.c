04c583b4546f4ef8179250ebeb44f781699f5733
Ali Umar <ali.umar.work@gmail.com>
2021-12-10 21:00:05 -0500

    rename pbuffer

--- src/particleBuffer.c
+++ src/particleBuffer.c
@@ -1,14 +1,14 @@
 #include "../include/particleBuffer.h"
 
-PARTICLE_BUFFER* init_particles_buffer()
+ParticleBuffer* init_particles_buffer()
 {
-    PARTICLE_BUFFER* buffer = calloc(1, sizeof(PARTICLE_BUFFER));
+    ParticleBuffer* buffer = calloc(1, sizeof(ParticleBuffer));
     buffer->particles       = NULL;
     buffer->capacity        = 0;
     return buffer;
 }
 
-void add_particle(PARTICLE_BUFFER* buffer, Particle* p)
+void add_particle(ParticleBuffer* buffer, Particle* p)
 {
     if (buffer->particles == NULL)
     {
@@ -32,55 +32,47 @@ void add_particle(PARTICLE_BUFFER* buffer, Particle* p)
     }
 }
 
-void remove_finished_particles(PARTICLE_BUFFER* buffer)
+void remove_finished_particles(ParticleBuffer* buffer)
 {
     // we need to scan through the buffer and look for particles with lifeTime = 0;
     // then we need to remove them from the buffer
     // if we remove we also need to shift the rest
-    if(buffer->size == 0)
+    if (buffer->size == 0)
         return;
 
     Particle* pArray = buffer->particles;
-    
+
     int i;
     for (i = 0; i < buffer->size; ++i)
     {
-        while(pArray[i].lifeTime == 0)
+        if (pArray[i].lifeTime == 0)
         {
-            if(i == buffer->size) {
-                if(buffer->size > 0)
-                    buffer->size--;
-                return;
-            }
-
             int j;
-            for (j = i; j < buffer->size; ++j)
-            {
+            for(j = i; j < buffer->size; ++j)
                 pArray[j] = pArray[j + 1];
-            }
             --buffer->size;
         }
-        
     }
 }
 
-void log_particles(PARTICLE_BUFFER* buffer)
+void log_particles(ParticleBuffer* buffer)
 {
     Particle* pArray = buffer->particles;
-    
+
     printf("Capacity: %d Size: %d\n", buffer->capacity, buffer->size);
 
     int i;
     for (i = 0; i < buffer->size; ++i)
     {
-        printf("index: %d x: %d y: %d vel: %lf acc: %lf lT: %lf color: (%d, %d, %d, %d)\n", i, pArray[i].pos.x, pArray[i].pos.y, pArray[i].vel,
-               pArray[i].acceleration, pArray[i].lifeTime, pArray[i].color.r, pArray[i].color.g, pArray[i].color.b, pArray[i].color.a);
+        printf("index: %d x: %d y: %d vel: %lf acc: %lf lT: %lf color: (%d, %d, %d, %d)\n", i, pArray[i].pos.x,
+               pArray[i].pos.y, pArray[i].vel, pArray[i].acceleration, pArray[i].lifeTime, pArray[i].color.r,
+               pArray[i].color.g, pArray[i].color.b, pArray[i].color.a);
     }
 
     printf("\n\n");
 }
 
-void destroy_particle_buffer(PARTICLE_BUFFER* buffer)
+void destroy_particle_buffer(ParticleBuffer* buffer)
 {
     free(buffer->particles);
     free(buffer);
