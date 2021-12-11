d3abf944a51019f9df9d8b8d77bbf0e3882cd7f2
Ali Umar <ali.umar.work@gmail.com>
2021-12-11 11:21:36 -0500

    update interface for linkedlist

--- include/particles.h
+++ include/particles.h
@@ -2,21 +2,21 @@
 #define PARTICLES_H
 
 #include "../include/raylib.h"
-#include "../include/particleBuffer.h"
+#include "../include/ParticleList.h"
 #include "../include/menu.h"
 
 #define RADIUS 2
-#define SIZE 1000 
+#define SIZE 100 
 
 
-void create_particles(ParticleBuffer*);
-void update_particles(ParticleBuffer*);
-void lifetime_stuff(Particle*, int);
-void apply_physics_quantities(Particle*, int);
-void check_bounds(Particle*, int);
+void create_particles(ParticleList*);
+void update_particles(ParticleList*);
+void lifetime_stuff(Particle*);
+void apply_physics_quantities(Particle*);
+void check_bounds(Particle*);
 void change_color_on_button_click();
 void change_velocity_on_button_click();
-void draw_particles(ParticleBuffer*);
-void destroy_particles(ParticleBuffer*);
+void draw_particles(ParticleList*);
+void destroy_particles_list(ParticleList*);
 
 #endif
